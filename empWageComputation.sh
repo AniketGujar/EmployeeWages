57447f11450b394db2a54d1cf1a1a4f42dfe49d1
AniketGujar <gujaraniket7875@gmail.com>
2020-11-02 17:31:21 +0530

    Refactor Code

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,40 +4,28 @@ DailyWage=160
 HalfDayWage=80
 TotalWage=0
 HourlyWage=20
+Attendance()
+{
+        attend=$((RANDOM%7))
+}
 
+TotalWages()
+{
+        TotalWage=$(($Totalwage+$wage))
+}
+
+echo "Day No        Wage        Total Wage"
 for (( i=1; i<=20; i++ ))
 do
-
-Attendance=$((RANDOM%7))
-
-	case $Attendance in
-	0)      Absent=$(($Absent+1))
-                echo "Day $i    00      $TotalWage" ;;
-
-        6)      HalfDay=$(($HalfDay+1))
-                TotalWage=$(($TotalWage+$HalfDayWage))
-                echo "Day $i    $HalfDayWage      $TotalWage" ;;
-
-	5)	OverTimeHours=$((RANDOM%4))
-		OverTHrs=$OverTimeHours
-
-		case $OverTimeHours in
-		0) OverTimeWage=$((4*20))
-		TotalWage=$(($TotalWage+$HalfDayWage))
-		echo "Day $i	$OverTime	$TotalWage";;
-		1) OverTime=$((3*20))
-                TotalWage=$(($TotalWage+$HalfDayWage))
-                echo "Day $i    $OverTime       $TotalWage";;
-		2) OverTime=$((2*20))
-                TotalWage=$(($TotalWage+$HalfDayWage))
-                echo "Day $i    $OverTime       $TotalWage";;
-		3) OverTime=20
-                TotalWage=$(($TotalWage+$HalfDayWage))
-                echo "Day $i    $OverTime       $TotalWage";;
-		esac	;;
-
-	*)      Present=$(($Present+1))
-                TotalWage=$(($TotalWage+$DailyWage))
-                echo "Day $i    $DailyWage      $TotalWage" ;;
+         case "$attendance" in
+                "1")
+                TotalWage $wage
+                echo "Day $d         $wage        $TotalWage"  ;;
+                "2")
+                TotalWage $wage
+                echo "Day $d         $wage        $TotalWage"  ;;
+                *)
+                TotalWage $wage
+                echo "Day $d         $wage        $TotalWage" ;;
         esac
 done
