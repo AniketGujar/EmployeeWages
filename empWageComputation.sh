836adfe908dbbd25fd3d1d98cc8c17f13b9314e7
AniketGujar <gujaraniket7875@gmail.com>
2020-11-02 17:05:14 +0530

    Wage Calcuation for a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,14 +5,38 @@ HalfDayWage=80
 TotalWage=0
 HourlyWage=20
 
-attendance=$((RANDOM%7))
+for (( i=1; i<=20; i++ ))
+do
+Attendance=$((RANDOM%7))
 
-if [[ $attendance -eq 1 ]]
-then
-	echo "Employee is Present"
-elif [[ $attendance -eq 6 ]]
-then
-	echo "OverTime"
-else
-        echo "Employee is Absent"
-fi
+	case $Attendance in
+	0)      Absent=$(($Absent+1))
+                echo "Day $i    00      $TotalWage" ;;
+
+        6)      HalfDay=$(($HalfDay+1))
+                TotalWage=$(($TotalWage+$HalfDayWage))
+                echo "Day $i    $HalfDayWage      $TotalWage" ;;
+
+	5)	OverTimeHours=$((RANDOM%4))
+		OverTHrs=$OverTimeHours
+
+		case $OverTimeHours in
+		0) OverTimeWage=$((4*20))
+		TotalWage=$(($TotalWage+$HalfDayWage))
+		echo "Day $i	$OverTime	$TotalWage";;
+		1) OverTime=$((3*20))
+                TotalWage=$(($TotalWage+$HalfDayWage))
+                echo "Day $i    $OverTime       $TotalWage";;
+		2) OverTime=$((2*20))
+                TotalWage=$(($TotalWage+$HalfDayWage))
+                echo "Day $i    $OverTime       $TotalWage";;
+		3) OverTime=20
+                TotalWage=$(($TotalWage+$HalfDayWage))
+                echo "Day $i    $OverTime       $TotalWage";;
+		esac	;;
+
+	*)      Present=$(($Present+1))
+                TotalWage=$(($TotalWage+$DailyWage))
+                echo "Day $i    $DailyWage      $TotalWage" ;;
+        esac
+done
